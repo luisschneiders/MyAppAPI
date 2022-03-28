e5c3f9fc5164942e4fab8a448cb7d12427d0d6ae
Luis Schneiders <schneiders.tech@gmail.com>
2022-03-29 00:52:42 +1100

    add controller and service for Language

---
+++ HealthCareAPI/Controllers/LanguageController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using HealthCareAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HealthCareAPI.Controllers
+{
+	[ApiController]
+	[Route("/api/v1/[controller]")]
+	public class LanguageController : Controller
+	{
+		[HttpGet(Name = "GetLanguages")]
+		public async Task<string> GetAsync()
+		{
+
+			var languages = await LanguageService.GetAsync();
+
+			return languages;
+		}
+	}
+}
+
---
+++ HealthCareAPI/Services/LanguageService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace HealthCareAPI.Services
+{
+	public class LanguageService
+	{
+
+        public static async Task<string> GetAsync()
+        {
+            var endpoint = Environment.GetEnvironmentVariable("X_RAPIDAPI_TRANSLATOR_ENDPOINT");
+            var route = Environment.GetEnvironmentVariable("X_RAPIDAPI_TRANSLATOR_ROUTE");
+
+            var x_rapidapi_host = Environment.GetEnvironmentVariable("X_RAPIDAPI_HOST");
+            var x_rapidapi_key = Environment.GetEnvironmentVariable("X_RAPIDAPI_KEY");
+
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+
+                RequestUri = new Uri(endpoint + route),
+
+                Headers =
+                {
+                    { "x-rapidapi-host", x_rapidapi_host },
+                    { "x-rapidapi-key", x_rapidapi_key },
+                },
+            };
+
+
+            var response = await client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+
+            var body = await response.Content.ReadAsStringAsync();
+
+            return body;
+        }
+    }
+}
