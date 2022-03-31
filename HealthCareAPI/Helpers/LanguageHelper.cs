1c72d98acd73d7e0349c7131622f259bbf87c913
Luis Schneiders <schneiders.tech@gmail.com>
2022-04-01 07:40:56 +1100

    add LanguageHelper and AppSettings

---
+++ HealthCareAPI/Helpers/LanguageHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using HealthCareAPI.Settings.Enum;
+
+namespace HealthCareAPI.Helpers
+{
+    public class LanguageHelper
+    {
+        public List<string> Scopes { get; set; } = new List<string>();
+
+        public LanguageHelper()
+        {
+            Scopes = ListOfScopes();
+        }
+
+        public List<string> ListOfScopes()
+        {
+            return Enum.GetNames(typeof(LanguageScope)).ToList();
+        }
+    }
+}
---
+++ HealthCareAPI/Shared/AppSettings.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using HealthCareAPI.Settings.Enum;
+
+namespace HealthCareAPI.Shared
+{
+    public class AppSettings
+    {
+        public static string scopeLanguage { get; set; } = "";
+
+        public string BuildScope(LanguageScope scope)
+        {
+            switch (scope)
+            {
+                case LanguageScope.dictionary:
+                    scopeLanguage = "dictionary";
+                    break;
+                case LanguageScope.translation:
+                    scopeLanguage = "translation";
+                    break;
+                case LanguageScope.transliteration:
+                    scopeLanguage = "transliteration";
+                    break;
+                case LanguageScope.all:
+                    scopeLanguage = "all";
+                    break;
+            }
+
+            return scopeLanguage;
+        }
+    }
+}
+
