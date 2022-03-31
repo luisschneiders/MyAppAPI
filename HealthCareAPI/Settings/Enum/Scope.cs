5bd0f853f81ee5d33c0568450166b5929ed8ab7b
Luis Schneiders <schneiders.tech@gmail.com>
2022-04-01 07:36:43 +1100

    rename enum file from Language to Scope

---
+++ HealthCareAPI/Settings/Enum/Scope.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace HealthCareAPI.Settings.Enum
+{
+	public enum LanguageScope
+	{
+
+		[Description("all")]
+		all = 0,
+
+		[Description("dictionary")]
+		dictionary = 1,
+
+		[Description("translation")]
+		translation = 2,
+
+		[Description("transliteration")]
+		transliteration = 3,
+
+	}
+}
