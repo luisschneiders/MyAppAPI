8d35d0f8d801f8a7b52267f969b90555271c7464
Luis Schneiders <schneiders.tech@gmail.com>
2022-04-23 16:12:11 +1000

    update LanguageService interface and class

--- HealthCareAPI/Services/LanguageService.cs
+++ HealthCareAPI/Services/LanguageService.cs
@@ -5,17 +5,14 @@ namespace HealthCareAPI.Services
 {
 	public class LanguageService : ILanguageService
 	{
-        private static LanguageHelper _languageHelper { get; set; } = new();
-
         private string _uri { get; set; } = "";
-        private bool _hasParameter { get; set; }
 
-        public async Task<string> GetAsync(string parameter)
+        public async Task<string> GetLanguagesAsync()
         {
             var x_rapidapi_host = Environment.GetEnvironmentVariable("X_RAPIDAPI_HOST");
             var x_rapidapi_key = Environment.GetEnvironmentVariable("X_RAPIDAPI_KEY");
 
-            BuildURI(parameter);
+            BuildURI();
 
             var client = new HttpClient();
             var request = new HttpRequestMessage
@@ -39,21 +36,50 @@ namespace HealthCareAPI.Services
             return body;
         }
 
-        public void BuildURI(string parameter)
+        public async Task<string> GetLanguagesByScopeAsync(string parameter)
+        {
+            var x_rapidapi_host = Environment.GetEnvironmentVariable("X_RAPIDAPI_HOST");
+            var x_rapidapi_key = Environment.GetEnvironmentVariable("X_RAPIDAPI_KEY");
+
+            BuildURIWithParameter(parameter);
+
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+
+                RequestUri = new Uri(_uri),
+
+                Headers =
+                {
+                    { "x-rapidapi-host", x_rapidapi_host },
+                    { "x-rapidapi-key", x_rapidapi_key },
+                },
+            };
+
+            var response = await client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+
+            var body = await response.Content.ReadAsStringAsync();
+
+            return body;
+        }
+
+        public void BuildURI()
         {
             var endpoint = Environment.GetEnvironmentVariable("X_RAPIDAPI_TRANSLATOR_ENDPOINT");
             var route = Environment.GetEnvironmentVariable("X_RAPIDAPI_TRANSLATOR_ROUTE");
 
-            CheckParameter(parameter);
-
-            _uri = _hasParameter ? $"{endpoint}{route}&scope={parameter}" : $"{endpoint}{route}";
+            _uri = $"{endpoint}{route}";
         }
 
-        public void CheckParameter(string parameter)
+        public void BuildURIWithParameter(string parameter)
         {
-            var scopes = _languageHelper.ListOfScopes();
+            var endpoint = Environment.GetEnvironmentVariable("X_RAPIDAPI_TRANSLATOR_ENDPOINT");
+            var route = Environment.GetEnvironmentVariable("X_RAPIDAPI_TRANSLATOR_ROUTE");
 
-            _hasParameter = parameter.Length > 0 && parameter != scopes[0];
+
+            _uri = $"{endpoint}{route}&scope={parameter}";
         }
     }
 }
